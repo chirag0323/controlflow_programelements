cc040dd204ddf7d3c49063c93cd633f772ca195e
CHIRAG GALOTRA <158015941+chirag0323@users.noreply.github.com>
2025-09-09 14:17:37 +0530

    Update q1.java

--- level 1/q1.java
+++ level 1/q1.java
@@ -1,4 +1,4 @@
-//Write a program to check if a number is divisible by 5  I/P => number O/P => Is the number ___ divisible by 5__-
+
 import java.util.Scanner;
 public class q1 {
   public static void main(String[] args) {
